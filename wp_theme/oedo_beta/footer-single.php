1e8c0e5949b7678f16ac2f5a4a9508782a6dd7d7
KuniyukiIIZAWA <info@921138.com>
2015-05-15 13:08:03 +0900

    β版アップに作成したファイルです

---
+++ wp_theme/oedo_beta/footer-single.php
@@ -0,0 +1,15 @@
+<!--footer.php-->
+<div id="footer">
+	<div id="inner-footer">
+		<p id="copy">copyrights code for oedo</p>
+	</div><!--/inner-footer-->
+</div><!--/footer-->
+
+<div id="pagetop-btn">
+	<p><a href="#pagetop">^</a></p>
+</div>
+<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/common.js"></script>
+<?php wp_foot(); ?>
+</body>
+</html>
---
+++ wp_theme/oedo_beta/single.php
@@ -0,0 +1,19 @@
+<?php get_header('single'); ?>
+<?php the_post(); ?>
+<ol id="bred">
+	<li id="home"><a href="/">Home</a></li>
+	<li><?php the_field('event_title'); ?></li>
+</ol>
+
+<div class="headings-area">
+	<h1 id="sec01-headings" class="headings"><?php the_field('event_title'); ?></h1>
+	<p class="lead"><?php the_time('Y年n月j日'); ?> Posted</p>
+</div>
+<div id="single-main">
+	<div id="single-post">
+		<?php the_content(); ?>
+		<p id="go-top"><a href="/">トップページに戻る &gt;</a></p>
+	</div><!--/single-post-->
+</div><!--main-->
+
+<?php get_footer('post'); ?>
