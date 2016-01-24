b17a27b77c5ec56032ee95c61df5f64ccccf4423
Fumio Furukawa <jumilla@codeforoedo.org>
2016-01-24 14:04:04 +0900

    アプリケーションリストのレイアウト修正。

--- wp-content/themes/oedo/blade/front-page-applications.blade.php
+++ wp-content/themes/oedo/blade/front-page-applications.blade.php
@@ -4,8 +4,11 @@
 	'post_status' => 'publish',
 	'post_type' => 'applications',
 	'posts_per_page' => -1,
+	'orderby' => 'meta_value',
+	'meta_key' => 'display_order',
+	'order' => 'ASC',
 ])
-		<li class="col-sm-12 col-xs-12">
+		<li class="col-md-4 col-sm-12 col-xs-12">
 			<div><a href="{!! get_field('application_url') !!}">
 				<img src="{!! get_field('icon') !!}" alt="{{ get_field('name') }}">
 				<br>
